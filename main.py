a33a42ae7253f525da93668ce51395cb89d879fb
mishhuang <mhuang987@gmail.com>
2025-11-05 18:18:38 -0500

    feat: add indexing pipeline with document embedding and storage

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+from typing import List
+
 from datasets import load_dataset
-from haystack import Document
+from haystack import Document, Pipeline
+from haystack.components.embedders import SentenceTransformersDocumentEmbedder
+from haystack.components.writers import DocumentWriter
+from haystack.document_stores.in_memory import InMemoryDocumentStore
+from haystack.document_stores.types import DuplicatePolicy
 
 dataset = load_dataset("vblagoje/PubMedQA_instruction", split="train")
 dataset = dataset.select(range(1000))
 all_documents = [Document(content=doc["context"]) for doc in dataset]
 all_questions = [doc["instruction"] for doc in dataset]
 all_ground_truth_answers = [doc["response"] for doc in dataset]
+
+document_store = InMemoryDocumentStore()
+
+document_embedder = SentenceTransformersDocumentEmbedder(model="sentence-transformers/all-MiniLM-L6-v2")
+document_writer = DocumentWriter(document_store=document_store, policy=DuplicatePolicy.SKIP)
+
+indexing = Pipeline()
+indexing.add_component(instance=document_embedder, name="document_embedder")
+indexing.add_component(instance=document_writer, name="document_writer")
+
+indexing.connect("document_embedder.documents", "document_writer.documents")
+
+indexing.run({"document_embedder": {"documents": all_documents}})
